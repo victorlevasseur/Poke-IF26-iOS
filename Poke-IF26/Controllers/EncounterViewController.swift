77be52ed250cc8de1a6be633182bcd7f5558ec94
user134638 <user134638@eu247.local>
2018-01-01 17:36:38 +0000

    Load the pokemon API data into the encounter view

--- Poke-IF26/Controllers/EncounterViewController.swift
+++ Poke-IF26/Controllers/EncounterViewController.swift
@@ -13,12 +13,33 @@ class EncounterViewController: UIViewController {
     
     var pokemon: Pokemon?
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameView: UILabel!
+    @IBOutlet weak var capturabilityView: UIProgressView!
+    @IBOutlet weak var captureButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         guard let encounteredPokemon = pokemon else {
             fatalError("EncounterViewController must be initialized with a pokemon!")
         }
+        
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        let _ = PokemonsService.getInstance().fetchPokemon(pokemon: encounteredPokemon)
+            .subscribe(onSuccess: { (fetchedPokemon) in
+                // Update the UI with the pokemon info.
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                
+                self.imageView.image = fetchedPokemon.pokemonBitmap
+                self.nameView.text = fetchedPokemon.pokemonName
+                self.capturabilityView.progress = Float(fetchedPokemon.pokemon.capturability)
+                self.captureButton.isHidden = false
+            }) { (err) in
+                // Can't fetch the pokemon, pop back to the map.
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                self.navigationController?.popViewController(animated: true)
+            }
     }
     
     override func didReceiveMemoryWarning() {
